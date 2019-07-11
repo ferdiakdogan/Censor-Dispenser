17c542e7bb9f98ec672feab8bfc57ff4ddd961d1
Ferdi Akdogan <ferdiakdogan@outlook.com>
2019-07-12 02:49:11 +0300

    Phrase censor is improved so it can censor list of phrases.

--- censor_dispenser.py
+++ censor_dispenser.py
@@ -7,18 +7,32 @@ email_four = open("email_four.txt", "r").read()
 
 def phrase_censor(mail, phrase):
     mail_remaining = mail
-    censored_mail = ""
+    censored_mail_one = ""
 
     while phrase in mail_remaining:
         phrase_start_index = mail_remaining.find(phrase)
         phrase_end_index = phrase_start_index + len(phrase)
-        censored_mail += mail_remaining[:phrase_start_index] + "&&&" + mail_remaining[phrase_start_index:phrase_end_index] + "&&&"
+        censored_mail_one += mail_remaining[:phrase_start_index] + "&&&" + mail_remaining[phrase_start_index:phrase_end_index] + "&&&"
         mail_remaining = mail_remaining[phrase_end_index:]
 
-    censored_mail += mail_remaining
+    censored_mail_one += mail_remaining
 
-    return censored_mail
+    return censored_mail_one
 
 
 censored_mail = phrase_censor(email_one, "learning algorithms")
-print(censored_mail)
+#print(censored_mail)
+
+
+def list_of_words_censor(mail, phrase_list):
+    censored_mail_two = mail
+    for phrase in phrase_list:
+        censored_mail_two = phrase_censor(censored_mail_two, phrase)
+
+    return censored_mail_two
+
+
+proprietary_terms = ["she", "personality matrix", "sense of self", "self-preservation", "learning algorithm", "her",
+                     "herself"]
+censored_mail_listed = list_of_words_censor(email_two, proprietary_terms)
+print(censored_mail_listed)
